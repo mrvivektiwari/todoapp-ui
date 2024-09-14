7e3b1341f25db669526394019414a954cc970ded
Vivek Tiwari <vivek@Viveks-MacBook-Pro.local>
2024-09-14 17:21:10 +0530

    add and view task: define state values and react click events

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,19 +3,39 @@ import { useState } from "react";
 
 function Todo() {
   const [taskInput, setTaskInput] = useState("");
+  const [tasks, setTasks] = useState(["s"]);
 
   const handleChange = (e) => {
     setTaskInput(e.target.value);
+  };
+
+  const addTask = () => {
+    if (taskInput) {
+      setTasks([...tasks, taskInput]);
+      setTaskInput("");
+    }
   };
 
   return (
     <div className="App">
       <h1>Todo Application</h1>
       <input onChange={handleChange} value={taskInput} id="task" name="task" />
-      <button id="taskButton" onclick="addTask()">
+      <button id="taskButton" onClick={addTask}>
         Add
       </button>
-      <ul id="todolist"></ul>
+      <ul id="todolist">
+        {tasks.map((task) => (
+          <>
+            <li key={task}>{task}</li>
+            <button className="editButton" onClick="editTask()">
+              Edit
+            </button>
+            <button className="deleteButton" onClick="removeTask()">
+              Remove
+            </button>
+          </>
+        ))}
+      </ul>
     </div>
   );
 }
